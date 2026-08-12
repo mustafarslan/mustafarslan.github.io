db3e446a48cee8b08282903b3a69e767c0394fab
mustafarslan <mustafarslan35@gmail.com>
2026-08-12 11:30:14 +0300

    Cv menu has been updated with Certifications

---
+++ assets/talks.index-6KbCEU0t.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-BKllkxft.js";import{j as t}from"./preload-helper-C1l--VWW.js";import{a as n}from"./index-CWbjw1AT.js";import{n as r}from"./TalkListPage-1R8-tESH.js";var i=e();function a(){let{talks:e}=n.useLoaderData(),{page:a}=n.useSearch(),o=t({from:n.fullPath});return(0,i.jsx)(r,{talks:e,page:a,onPageChange:e=>o({search:{page:e}})})}export{a as component};
